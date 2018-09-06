61a8f14cefaf6ee295616be719797d8a4679b908
Vinicius N. Coscia <vinicius.coscia@live.com>
2018-09-06 08:33:48 -0300

    first commit

---
+++ src/main/java/br/com/vinicius/aplicacaoRestVinicius/aplicacaoRestVinicius/AplicacaoRestViniciusApplication.java
@@ -0,0 +1,12 @@
+package br.com.vinicius.aplicacaoRestVinicius.aplicacaoRestVinicius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoRestViniciusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoRestViniciusApplication.class, args);
+	}
+}
